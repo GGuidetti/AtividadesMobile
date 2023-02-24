4cfd13c0c5738d5d85a03c5de1a282f0ac61b660
Luis Fernando Brasil <luis4756@live.com>
2023-02-24 20:32:07 -0300

    Exercicios 24-02

---
+++ dart_application_1/bin/ex1.dart
@@ -0,0 +1,14 @@
+import 'package:dart_application_1/dart_application_1.dart'
+    as dart_application_1;
+
+void main(List<String> arguments) {
+  String nome = "Macarrão de sêmola";
+  String categoria = "Alimento";
+  double preco = 9.35;
+  bool disponivel = true;
+
+  print('Produto: ${nome}');
+  print('Categoria: ${categoria}');
+  print('Preço: ${preco}');
+  print('Disponível: ${disponivel}');
+}
---
+++ dart_application_1/bin/ex2.dart
@@ -0,0 +1,14 @@
+import 'package:dart_application_1/dart_application_1.dart'
+    as dart_application_1;
+
+void main(List<String> arguments) {
+  String filme = "Avatar 2";
+  String genero = "Aventura";
+  double preco = 24.0;
+  bool lotado = false;
+
+  print('Filme: ${filme}');
+  print('Genero: ${genero}');
+  print('Preço: ${preco}');
+  print('Sessão lotada: ${lotado}');
+}
